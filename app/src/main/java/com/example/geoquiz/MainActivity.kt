1d05bc4bd3bef5cbabe46936af50256156a896e9
Sheykin <sheik80@gmail.com>
2019-12-06 18:34:14 +0900

    list 6.17

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.geoquiz
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,6 +19,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
+private  const val REQUEST_CODE_CHEAT = 0
 
 
 class MainActivity : AppCompatActivity() {
@@ -86,14 +88,29 @@ class MainActivity : AppCompatActivity() {
 
         cheatButton.setOnClickListener(){
             //start CheatActivity
-            val intent = Intent(this, CheatActivity::class.java)
-            startActivity(intent)
+            //val intent = Intent(this, CheatActivity::class.java)
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            //startActivity(intent)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
         }
 
         updateQuestion()
         checkPersent()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK){
+            return
+        }
+        if (requestCode == REQUEST_CODE_CHEAT){
+            quizViewModel.isCheater =
+                data?.getBooleanExtra(EXTRA_ANSWER_SHOWN,false) ?: false
+        }
+
+    }
 
     override fun onStart() {
         super.onStart()
@@ -137,6 +154,13 @@ class MainActivity : AppCompatActivity() {
     private fun  checkAnswer(userAnswer: Boolean){
         val correctAnswer = quizViewModel.currentQuestionAnswer
 
+
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer ->R.string.correct_toast
+            else -> R.string.incorrect_toast
+        }
+/*
         val messageResId = if (userAnswer == correctAnswer){
             countTrueAnswer++
             R.string.correct_toast
@@ -146,6 +170,7 @@ class MainActivity : AppCompatActivity() {
         else{
             R.string.incorrect_toast
         }
+*/
 
         val toast = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
         toast.setGravity(Gravity.TOP, 0, 300)
